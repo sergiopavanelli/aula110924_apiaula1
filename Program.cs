774ea0b9a9e95ed1a6c4696544e24bf6ce8b4cbe
Sérgio Pavanelli <sergiopavanelli@gmail.com>
2024-09-11 11:03:16 -0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+//https://github.com/alexmontanha/tutorial_api_dotnet
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
