3274a52f0669733c5975161718a8a6178e194e01
Sérgio Pavanelli <sergiopavanelli@gmail.com>
2024-09-11 11:22:33 -0300

    new data

--- Program.cs
+++ Program.cs
@@ -6,5 +6,14 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/hello", (string name) =>
+
+{
+    var saudacao = DateTime.Now.Hour < 12 ? "Bom dia" : "Boa tarde";
+    var retorno = $"{saudacao}, {name}";
+    return retorno;
+});
+
 app.Run();
 
+// http://localhost:{port}/hello?name=Montanha
--- obj/Debug/net8.0/ApiAula1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiAula1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiAula1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abaaf7327d1b33a8554bf1cf4d01fd4e978933e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774ea0b9a9e95ed1a6c4696544e24bf6ce8b4cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiAula1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiAula1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
